73bd587a64d262bc98a4d0784eb77d95a1141282
Aditya Kresna <aditya.kresna@outlook.co.id>
2020-08-23 18:43:12 +0000

    Python3 binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use crossbeam_queue::ArrayQueue;
-use pyo3::exceptions::TypeError;
 use pyo3::prelude::*;
 use pyo3::types::PyByteArray;
 use std::net::{SocketAddr, UdpSocket};
@@ -13,11 +12,37 @@ pub const MAX_PENDING_TX: usize = 128;
 pub const MAX_PENDING_RX: usize = 128;
 pub const MAX_PACKET_SIZE: usize = 1460;
 
+#[pyclass(freelist = 1024)]
+#[derive(Clone, Debug)]
 pub struct UdpPacket {
     pub peer: SocketAddr,
     pub data: Vec<u8>,
 }
 
+#[pymethods]
+impl UdpPacket {
+    #[new]
+    pub fn new(peer_ip_address: &str, peer_port: u16) -> Self {
+        Self {
+            peer: SocketAddr::new(peer_ip_address.parse().unwrap(), peer_port),
+            data: Vec::new(),
+        }
+    }
+
+    #[getter]
+    pub fn get_data<'a>(&self, py: Python<'a>) -> PyResult<&'a PyByteArray> {
+        Ok(PyByteArray::new(py, &self.data[..]))
+    }
+
+    #[setter]
+    pub fn set_data(&mut self, raw_bytes: &PyByteArray) -> PyResult<()> {
+        self.data = raw_bytes.to_vec();
+
+        Ok(())
+    }
+}
+
+#[pyclass]
 pub struct UdpLoop {
     stop_flag: Arc<AtomicBool>,
     pending_tx: Arc<ArrayQueue<UdpPacket>>,
@@ -36,29 +61,40 @@ impl Default for UdpLoop {
     }
 }
 
+#[pymethods]
 impl UdpLoop {
+    #[new]
     pub fn new() -> Self {
         Default::default()
     }
 
-    pub fn try_receive(&self) -> Option<UdpPacket> {
-        self.pending_rx.pop().ok()
+    pub fn try_receive(&self) -> PyResult<Option<UdpPacket>> {
+        match self.pending_rx.pop() {
+            Err(_) => Ok(None),
+            Ok(packet) => Ok(Some(packet)),
+        }
     }
 
-    pub fn transmit(&self, packet: UdpPacket) -> bool {
+    pub fn transmit(&self, packet: UdpPacket) -> PyResult<bool> {
         if let Err(_) = self.pending_tx.push(packet) {
-            return false;
+            return Ok(false);
         }
 
-        true
+        Ok(true)
     }
 
-    pub fn start(&mut self, listen_address: &str, listen_port: u16) {
+    pub fn start(&mut self, listen_address: &str, listen_port: u16) -> PyResult<bool> {
         let stop_flag = self.stop_flag.clone();
         let pending_tx = self.pending_tx.clone();
         let pending_rx = self.pending_rx.clone();
         let listen_address = listen_address.to_string();
-        *self.loop_handle.lock().unwrap() = Some(spawn_thread(move || {
+        let mut loop_handle = self.loop_handle.lock().unwrap();
+
+        if loop_handle.is_some() {
+            return Ok(false);
+        }
+
+        (*loop_handle) = Some(spawn_thread(move || {
             let loop_grace_duration = Duration::from_millis(LOOP_GRACE_DURATION_MS);
             let mut buffer_rx = [0u8; MAX_PACKET_SIZE];
             let binding_socket = SocketAddr::new(listen_address.parse().unwrap(), listen_port);
@@ -68,13 +104,19 @@ impl UdpLoop {
             let pending_rx = pending_rx;
 
             while !stop_flag.load(Ordering::Relaxed) {
-                sleep(loop_grace_duration);
+                if let Ok(new_tx_packet) = pending_tx.pop() {
+                    let _ = udp_socket.send_to(&new_tx_packet.data, new_tx_packet.peer);
+                }
 
                 match udp_socket.recv_from(&mut buffer_rx) {
-                    Err(_) => (),
+                    Err(_) => {
+                        if pending_tx.is_empty() {
+                            sleep(loop_grace_duration);
+                        }
+                    }
                     Ok((rx_length, peer)) => {
-                        if rx_length != buffer_rx.len() {
-                            eprintln!("Received packet beyond maximum size of {}!", MAX_PACKET_SIZE);
+                        if rx_length > buffer_rx.len() {
+                            eprintln!("Received packet beyond maximum size of {}", MAX_PACKET_SIZE);
                         } else {
                             let new_rx_packet = UdpPacket {
                                 peer,
@@ -84,21 +126,28 @@ impl UdpLoop {
                         }
                     }
                 }
-
-                if let Ok(new_tx_packet) = pending_tx.pop() {
-                    let _ = udp_socket.send_to(&new_tx_packet.data, new_tx_packet.peer);
-                }
             }
         }));
+
+        Ok(true)
     }
 
-    pub fn stop(&mut self) {
+    pub fn stop(&mut self) -> PyResult<bool> {
         match self.loop_handle.lock().unwrap().take() {
-            None => (),
+            None => Ok(false),
             Some(loop_handle) => {
                 self.stop_flag.store(true, Ordering::Relaxed);
                 let _ = loop_handle.join();
+                Ok(true)
             }
         }
     }
+}
+
+#[pymodule]
+fn py_udp_loop(_: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<UdpPacket>()?;
+    m.add_class::<UdpLoop>()?;
+
+    Ok(())
 }
